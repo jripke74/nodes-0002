7a996b8d3006e7aff52b2eb48fb3c66858de1be5
Jeff Ripke <jripke74@gmail.com>
2023-09-14 16:19:29 -0700

    add admin routes back:

--- util/database.js
+++ util/database.js
@@ -1,10 +1,19 @@
-const Sequelize = require('sequelize');
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
 const secrets = require("./secrets");
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const mongoConnect = () => {
+  MongoClient.connect(
+    `mongodb+srv://jeff:${secrets.mongoDbPassword}@cluster0.delsao3.mongodb.net/shop?retryWrites=true&w=majority`
+  )
+    .then((client) => {
+      console.log("Connected!!!");
+      callback(client);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
-module.exports = sequelize;
+module.exports = mongoConnect;
