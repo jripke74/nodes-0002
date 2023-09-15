94b49415a1a94059b2c46ada303783976d10dce2
Jeff Ripke <jripke74@gmail.com>
2023-09-15 16:30:58 -0700

    add edit logic

--- models/product.js
+++ models/product.js
@@ -2,18 +2,27 @@ const mongodb = require("mongodb");
 const getDb = require("../util/database").getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id = id;
   }
 
   save() {
     const db = getDb();
-    return db
-      .collection("products")
-      .insertOne(this)
+    let dbOp;
+    if (this._id) {
+      dbOp = db
+        .collection("products")
+        .updateOne({ _id: new mongodb.ObjectId(this._id) }, { $set: this });
+    } else {
+      dbOp = db
+        .collection("products")
+        .insertOne({ _id: db.collection("products").insertOne(this) });
+    }
+    return dbOp
       .then((result) => {
         console.log(result);
       })
@@ -43,7 +52,7 @@ class Product {
       .collection("products")
       .find({ _id: new mongodb.ObjectId(prodId) })
       .next()
-      .then(product => {
+      .then((product) => {
         console.log(product);
         return product;
       })
