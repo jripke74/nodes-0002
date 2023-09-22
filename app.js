15181c464b4ded791caa496b0c93032761d27e58
Jeff Ripke <jripke74@gmail.com>
2023-09-22 10:34:16 -0700

    add cart functionality

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   User.findById("6508c6660c0bc7c29e52efe4")
     .then((user) => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch((err) => console.log(err));
