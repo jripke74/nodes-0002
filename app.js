367b439b427936b2cef2ee1a428da9e211979fb8
Jeff Ripke <jripke74@gmail.com>
2023-09-27 14:44:11 -0700

    add user model add user to app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const mongoDbUsername = require("./util/secrets").mongoDbUsername;
 const mongoDbPassword = require("./util/secrets").mongoDbPassword;
 
 const errorController = require("./controllers/error");
-// const User = require("./models/user");
+const User = require("./models/user");
 
 const app = express();
 
@@ -22,13 +22,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  // User.findById("6508c6660c0bc7c29e52efe4")
-  //   .then((user) => {
-  //     req.user = new User(user.name, user.email, user.cart, user._id);
-  //     next();
-  //   })
-  //   .catch((err) => console.log(err));
-  next();
+  User.findById("6514a1f2b8081105760bbe4f")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
 });
 
 app.use("/admin", adminRoutes);
@@ -41,8 +40,20 @@ mongoose
     `mongodb+srv://${mongoDbUsername}:${mongoDbPassword}@cluster0.mxuwfw1.mongodb.net/shop?retryWrites=true&w=majority`
   )
   .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "Jeff",
+          email: "jeff@email.com",
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+    });
     app.listen(3000);
-    console.log('Connected!!!');
+    console.log("Connected!!!");
   })
   .catch((err) => {
     console.log(err);
